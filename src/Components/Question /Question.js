4cf9c25bee946389bf60b071c91fc3afdbb68bbf
Flannel Bear <josh.flannelbear@gmail.com>
2018-06-30 13:02:38 -0500

    set up local state of question component

--- src/Components/Question /Question.js
+++ src/Components/Question /Question.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
+import PropType from ''
 
 const mapReduxStateToProps = ({feedbackReducer}) => ({
     feedbackReducer
 });
 
 class Question extends Component{
+    constructor(){
+        super();
+
+        this.state = {input: ''}
+    }
+
+    
+
     render(){
         return(
             <div>
                 <h2>Question</h2>
+                <h4>{this.props.question}</h4>
+                <input type="text" value={this.state.input} onChange={this.handleInputChange} placeholder="Answer"/>
             </div>
         );
     }
