f53f96e1a17b546aa5ddebbe53f5f4c136384a83
Flannel Bear <josh.flannelbear@gmail.com>
2018-06-30 12:53:00 -0500

    import pages into App for use in Route components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,17 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 import { HashRouter as Router, Route} from 'react-router-dom';
 
+// Pages
+import Feeling from '../../Pages/Feeling';
+import Understand from '../../Pages/Understand';
+import Support from '../../Pages/Support';
+import Comments from '../../Pages/Comments';
+import Thankyou from '../../Pages/Thankyou';
+import Admin from '../../Pages/Admin';
+
+
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <Route exact path='/' component={}/>
-          <Route exact path='/' component={}/>
-          <Route exact path='/' component={}/>
-          <Route exact path='/' component={}/>
-          <Route exact path='/' component={}/>
-          <Route exact path='/' component={}/>
+          <Route exact path='/' component={Feeling}/>
+          <Route exact path='/' component={Understand}/>
+          <Route exact path='/' component={Support}/>
+          <Route exact path='/' component={Comments}/>
+          <Route exact path='/' component={Thankyou}/>
+          <Route exact path='/' component={Admin}/>
         </div>
       </Router>
     );
